455ac6bb48126f9b313b1dc12a22650bcb20939c
Eduardo <eduardoaandrad@Eduardos-MBP.lan>
2020-02-04 12:16:03 -0300

    novo template

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
-print("Hello World tste")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def hello():
+    return "Hello World tste"
+
+if __name__ == "__main__":
+    app.run()
