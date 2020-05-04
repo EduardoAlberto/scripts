014b825be72f55fa899b89ac31115f8fa959ddab
Eduardo <eduardoaandrad@Eduardos-MBP.lan>
2020-05-04 14:22:16 -0300

    add connect apache cassandra

--- etl05.py
+++ etl05.py
@@ -3,6 +3,8 @@
 import tweepy 
 import datetime as dt
 import pandas as pd
+from cassandra.cluster import Cluster
+
 
 
 
@@ -45,11 +47,59 @@ def arq(pesquisa):
 
 # pesquisa palavra no twitter
 # palavra = arq(input())
-palavra = arq(input('Digite uma palavra: '))
-txt = pd.DataFrame(palavra, columns=['usuario','localizacao','total_amigos','total_seguidores','total_listas','Total_likes','status_verificado','total_status','total_retweet','tweet'])
+pesquisa = input('Digite uma palavra: ')
+palavra = arq(pesquisa)
+txt = pd.DataFrame(palavra, columns=['usuario','localizacao','total_amigos','total_seguidores','total_listas','total_likes','status_verificado','total_status','total_retweet','tweet'])
 txt['localizacao'].replace({'\u0000', 'sem_localizacao'},inplace =True)
+
 # conexao com o banco
+cluster = Cluster(['127.0.0.1'])
+session = cluster.connect()
 
-print(txt)
+def acessa_banco(palavra):
+       session.execute("""CREATE KEYSPACE IF NOT EXISTS %s WITH replication = {'class': 'SimpleStrategy', 'replication_factor' :'1' }""" % palavra)
+       session.set_keyspace(palavra)
+       session.execute("""
+              create table if not exists stg_pesquisa_twitter(
+                      user_id           uuid  
+                     ,usuario           text
+                     ,localizacao       text
+                     ,total_amigos      int 
+                     ,total_seguidores  int 
+                     ,total_listas      int         
+                     ,total_likes       int          
+                     ,status_verificado boolean
+                     ,total_status      int  
+                     ,total_retweet     int  
+                     ,tweet             text
+                     ,dt_load           TIMESTAMP  
+                     ,PRIMARY KEY(user_id, usuario)       
+              ) WITH comment='Informacao dos twitter mais pesquisados';
+       """
+       )
 
+       # insert da tabela 
+       query = "insert into stg_pesquisa_twitter (user_id, usuario, localizacao, total_amigos, total_seguidores, total_listas, total_likes, status_verificado, total_status, total_retweet, tweet,dt_load ) values (now(), ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,dateOf(now()))"
+               
+       prepared = session.prepare(query)
+       for index, row in txt.iterrows():
+              session.execute(prepared
+                             ,(row['usuario']
+                             , row['localizacao']
+                             , row['total_amigos']
+                             , row['total_seguidores']
+                             , row['total_listas']
+                             , row['total_likes']
+                             , row['status_verificado']
+                             , row['total_status']
+                             , row['total_retweet']
+                             , row['tweet'])
+                             )             
+
+
+
+acessa_banco(pesquisa)
+print(txt[['usuario','localizacao', 'total_amigos', 'total_seguidores', 'total_listas', 'total_likes', 'status_verificado', 'total_status', 'total_retweet', 'tweet' ]])
+print(txt.dtypes)
+print(str(pesquisa))
 
